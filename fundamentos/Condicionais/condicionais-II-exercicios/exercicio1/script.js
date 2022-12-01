52229ec7683d4b494cca0e98a412ab09603f310d
suuzanemoura <[suuzanemoura@gmail.com]>
2022-12-01 15:15:55 -0300

    feat: Aula e Exercícios Condicionis II

--- fundamentos/Condicionais/condicionais-II-exercicios/exercicio1/script.js
+++ fundamentos/Condicionais/condicionais-II-exercicios/exercicio1/script.js
@@ -8,17 +8,16 @@ Crie um código que receba um número por prompt e verifique se um número é di
 
 let number = Number(prompt("Insira um número:"))
 
-if(number%2 === 0 || number%3 === 0){
-
-    if (number%2 === 0) {
-        console.log(number, "é divisível por 2.")    
-    }
-    
+if(number%2 === 0){
     if (number%3 === 0) {
-        console.log(number, "é divisível por 3.")
-    } 
-    
-} else{
+    console.log(number, "é divisível por 2 e por 3.")    
+    } else {
+    console.log(number, "é divisível por 2") 
+    }
+
+} else if (number%3 === 0) {
+    console.log(number, "é divisível por 3.")
+   } else{
     console.log(number, "não é divisível por 2 ou 3.")
 }
 
