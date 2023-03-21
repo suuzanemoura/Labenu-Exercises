3e96e4a9f6dbc517f246d8050037340d55eda2ff
suuzanemoura <[suuzanemoura@gmail.com]>
2023-03-21 11:40:21 -0300

    Exercícios da aula de Typescript

---
+++ backend/Typescript/typescript-I-template/src/tipando/index.ts
@@ -0,0 +1,15 @@
+import {frota, TCarro} from "./frota"
+
+function buscarCarrosPorMarca(frota: TCarro[], marca?:string) {
+  if (marca === undefined) {
+    return frota
+  }
+
+  return frota.filter(
+    (carro) => {
+      return carro.marca === marca
+    }
+  )
+}
+
+console.log(buscarCarrosPorMarca(frota)) 
