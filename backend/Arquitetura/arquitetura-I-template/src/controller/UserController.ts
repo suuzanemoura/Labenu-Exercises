673791418b6b28313818847a6d3813458f3ba5bf
suuzanemoura <[suuzanemoura@gmail.com]>
2023-04-24 15:11:48 -0300

    Exercícios da aula de Arquitetura

---
+++ backend/Arquitetura/arquitetura-I-template/src/controller/UserController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express"
+import { UserBusiness } from "../business/UserBusiness"
+
+export class UserController {
+    public getUsers = async (req: Request, res: Response) => {
+        try {
+            const q = req.query.q as string | undefined
+    
+            const userBusiness = new UserBusiness()
+            const users = await userBusiness.getUsers(q)
+    
+            res.status(200).send(users)
+        } catch (error) {
+            console.log(error)
+    
+            if (req.statusCode === 200) {
+                res.status(500)
+            }
+    
+            if (error instanceof Error) {
+                res.send(error.message)
+            } else {
+                res.send("Erro inesperado")
+            }
+        }
+    }
+
+    public createUser = async (req: Request, res: Response) => {
+        try {
+            const { id, name, email, password } = req.body
+
+            const input = {
+                id,
+                name,
+                email,
+                password
+            }
+
+            const userBusiness = new UserBusiness()
+            const output = await userBusiness.createUser(input)
+
+            res.status(201).send(output)
+
+        } catch (error) {
+            console.log(error)
+    
+            if (req.statusCode === 200) {
+                res.status(500)
+            }
+    
+            if (error instanceof Error) {
+                res.send(error.message)
+            } else {
+                res.send("Erro inesperado")
+            }
+        }
+    }
+}
