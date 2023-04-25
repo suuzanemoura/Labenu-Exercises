5dfc7e99b276e73e40cbbdfbceb15bcaa1791115
suuzanemoura <[suuzanemoura@gmail.com]>
2023-04-25 11:13:12 -0300

    Exercícios da aula de Arquitetura II

---
+++ backend/Arquitetura/arquitetura-II-template/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import { AccountController } from './controller/AccountController'
+import { userRouter } from './router/userRouter'
+import { accountRouter } from './router/accountRouter'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, () => {
+    console.log(`Servidor rodando na porta ${3003}`)
+})
+
+app.use("/users", userRouter)
+app.use("/accounts", accountRouter)
